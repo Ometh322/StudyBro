09d5c7dbad4590fd90cd172530deb19e50de055a
Daniil Ostroukhov <ostr.dan@yandex.ru>
2020-11-06 20:24:13 +0400

    Updates

---
+++ precache-manifest.09fe5741127adc9087592be73e3ce91e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6411ce295c676903202d8f0b214d8694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57147d8e5c7f90ad2846",
+    "url": "/static/css/2.8fde3e13.chunk.css"
+  },
+  {
+    "revision": "44dd639d0ed41e415c9c",
+    "url": "/static/css/main.646300c6.chunk.css"
+  },
+  {
+    "revision": "57147d8e5c7f90ad2846",
+    "url": "/static/js/2.ce814275.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ce814275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dd639d0ed41e415c9c",
+    "url": "/static/js/main.bedd0d82.chunk.js"
+  },
+  {
+    "revision": "8279fa74cae8149986f7",
+    "url": "/static/js/runtime-main.587629ae.js"
+  },
+  {
+    "revision": "3362bb5b6d07190efca83caa341f1b4c",
+    "url": "/static/media/student.3362bb5b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StudyBro/precache-manifest.0d4290fd05badb6642714254e212c5c1.js"
+  "/precache-manifest.09fe5741127adc9087592be73e3ce91e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StudyBro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
