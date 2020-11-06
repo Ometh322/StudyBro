9053aa80d3a80e38813be74b93299f929143ac5f
Daniil Ostroukhov <ostr.dan@yandex.ru>
2020-11-06 20:53:17 +0400

    Updates

---
+++ precache-manifest.83ecdd45a57474072765dfb8484368c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa2f2e2caee330e9f79e6fecaf0cd0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f288d960bfe3ce98e56b",
+    "url": "./static/css/2.8fde3e13.chunk.css"
+  },
+  {
+    "revision": "69c8e075d88c2a958723",
+    "url": "./static/css/main.c37d07a4.chunk.css"
+  },
+  {
+    "revision": "f288d960bfe3ce98e56b",
+    "url": "./static/js/2.0fef46f1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0fef46f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c8e075d88c2a958723",
+    "url": "./static/js/main.0b0d532f.chunk.js"
+  },
+  {
+    "revision": "64543ee9737b1e44c085",
+    "url": "./static/js/runtime-main.1d10575d.js"
+  },
+  {
+    "revision": "3362bb5b6d07190efca83caa341f1b4c",
+    "url": "./static/media/student.3362bb5b.jpg"
+  }
+]);
