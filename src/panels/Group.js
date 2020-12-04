0ab83210926e86da0606cb65b5af3fc302998359
Daniil Ostroukhov <ostr.dan@yandex.ru>
2020-12-04 22:29:25 +0400

    Added Group Panel

---
+++ src/panels/Group.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import { IOS, platform } from '@vkontakte/vkui';
+import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
+import group from '../img/student.jpg';
+const osName = platform();
+
+const Group = props => (
+    <Panel id={props.id}>
+        <PanelHeader
+            left={<PanelHeaderButton onClick={props.go} data-to="home">
+                {osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+            </PanelHeaderButton>}>
+            Список группы
+        </PanelHeader>
+        <img className="Group" src={group} alt="Group 311"/>
+    </Panel>
+)
+
+Group.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+};
+
+export default Group
