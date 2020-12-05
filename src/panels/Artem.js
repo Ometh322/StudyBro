7519a1ef2cbe9c1bd09379926295ed30309e5957
Ridersar <ak.arty@mail.ru>
2020-12-05 09:59:01 +0400

    Added Add News

--- src/panels/Artem.js
+++ src/panels/Artem.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+// import PropTypes from 'prop-types';
 import { platform, IOS } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
@@ -8,17 +8,48 @@ import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
 import ArticleList from "../components/ArticleList"
-import articles from "../components/fixtures"
+import articles1 from "../components/fixtures"
 
 import './Student.css';
+// import { render } from 'react-dom';
 
+function Persik(props) {
+const [articles, setArticles] = React.useState(articles1)
 const osName = platform();
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+  }
 
 const handleClick = () => {
 	console.log('---', 'clicked')
+	// setArticles([{
+	// 	id: 6, date: "2020-12-01", 
+	// 	title: "Практическая работа", 
+	// 	text: "Практическая работа по экономике"
+	// }])
+	setArticles(
+		articles.concat([{ //посмотреть что с ключами(id)
+				id:   getRandomInt(30, 1000000), date: "2020-12-01", 
+				title: "Практическая работа", 
+				text: "Практическая работа по экономике"
+			}])
+	  )
+
 };
 
-const Persik = props => (
+//добавление новости
+function addTodo(titleIn) {
+    setArticles(
+		articles.concat([{ //посмотреть что с ключами(id)
+				id:   getRandomInt(30, 1000000), date: "2020-12-05", 
+				title: titleIn, 
+				text: "Новость в разработке"
+			}])
+	  )
+  }
+
+return (
 	<Panel id={props.id}>
 		<PanelHeader
 			left={<PanelHeaderButton onClick={props.go} data-to="home">
@@ -30,9 +61,10 @@ const Persik = props => (
 		<div>
             <div>
                 <h1>
-                    Последние новости
+                    Последние 
+					<AddTodo onCreate={addTodo}/>
                     <button onClick={handleClick}>
-                        Какая-то кнопка
+                        Добавить новость
                     </button>
                 </h1>
             </div>
@@ -41,9 +73,29 @@ const Persik = props => (
 	</Panel>
 );
 
-Persik.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-};
+// Persik.propTypes = {
+// 	id: PropTypes.string.isRequired,
+// 	go: PropTypes.func.isRequired,
+// };
+
+function AddTodo({onCreate}) {
+	const [value, setValue] = useState('')
+
+	function submitHandler(event) {
+		event.preventDefault()
+
+		if (value.trim()) {
+			onCreate(value)
+		}
+	}
+
+    return (
+        <form style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
+            <input value={value} onChange={event => setValue(event.target.value)}/>
+            <button type="submit">Добавить свою новость</button>
+        </form>
+    )
+}
+}
 
 export default Persik;
