a16490999d3f7cfdb5723e56d03bf2b3c520ad24
Daniil Ostroukhov <ostr.dan@yandex.ru>
2020-12-12 01:48:25 +0400

    Labels update

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,7 +10,7 @@ import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Возможность погладить Васю</PanelHeader>
+		<PanelHeader>Study Bro</PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Bridge">
 			<Cell
