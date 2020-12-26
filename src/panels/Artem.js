ae4da0de80119709707746a7418084a24c23ffc5
Daniil Ostroukhov <ostr.dan@yandex.ru>
2020-12-26 12:29:44 +0400

    Added TimeTable

--- src/panels/Artem.js
+++ src/panels/Artem.js
@@ -1,34 +1,22 @@
 import React, {useState} from 'react';
-// import PropTypes from 'prop-types';
 import { platform, IOS} from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
-// import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import ArticleList from "../components/ArticleList"
-
-//import articles from "../components/fixtures"
 import CellButton from '@vkontakte/vkui/dist/components/CellButton/CellButton';
 import Icon16Add from '@vkontakte/icons/dist/16/add';
-// import IconNews from '@vkontakte/icons/dist/20/newsfeed_outline';
 import Separator from '@vkontakte/vkui/dist/components/Separator/Separator';
-// import View from '@vkontakte/vkui/dist/components/View/View';
-// import Group from '@vkontakte/vkui/dist/components/Group/Group';
-// import Header from '@vkontakte/vkui/dist/components/Header/Header';
-// import Div from '@vkontakte/vkui/dist/components/Div/Div';
-// import WriteBar from '@vkontakte/vkui/dist/components/WriteBar/WriteBar';
-// import WriteBarIcon from '@vkontakte/vkui/dist/components/WriteBarIcon/WriteBarIcon';
-// import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
+
 
 
 import articles1 from "../components/fixtures.json"
 
 
 import './Student.css';
-// import { set } from 'core-js/fn/dict';
-// import { render } from 'react-dom';
+
 
 function Persik(props) {
 const [articles, setArticles] = React.useState(articles1)
@@ -42,11 +30,6 @@ function getRandomInt(min, max) {
 
 const handleClick = () => {
 	console.log('---', 'clicked')
-	// setArticles([{
-	// 	id: 6, date: "2020-12-01", 
-	// 	title: "Практическая работа", 
-	// 	text: "Практическая работа по экономике"
-	// }])
 	let inf = { //посмотреть что с ключами(id)
 		id:   getRandomInt(30, 1000000), date: "2020-12-06", 
 		title: "Практическая работа", 
@@ -72,7 +55,6 @@ let state = {
 	// text: 'op'
 }
 
-// let text = state.textOpen
 
 //реверс списка
 const revert = () => {
@@ -145,11 +127,6 @@ return (
 	</Panel>
 );
 
-// Persik.propTypes = {
-// 	id: PropTypes.string.isRequired,
-// 	go: PropTypes.func.isRequired,
-// };
-
 function AddTodo({onCreate}) {
 	const [value, setValue] = useState('')
 
@@ -189,10 +166,5 @@ function SearchTodo({onCreate}) {
 }
 
 }
-
-
-
-  
-
 
 export default Persik;
