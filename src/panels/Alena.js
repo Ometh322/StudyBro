47865539f3129eb4c5a864a102e30e6a4beb36b7
OlFil <miss-olenka2000@yandex.ru>
2021-01-27 03:31:51 +0400

    фиксики

--- src/panels/Alena.js
+++ src/panels/Alena.js
@@ -5,8 +5,6 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import { platform, IOS, Panel, PanelHeader, PanelHeaderButton, List, Group, Cell } from '@vkontakte/vkui';
-
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import './Student.css';
