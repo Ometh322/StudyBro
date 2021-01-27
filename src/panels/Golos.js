281a2f0c18d94af727ee176c15daa097dcfcf5a3
OlFil <miss-olenka2000@yandex.ru>
2021-01-27 18:01:03 +0400

    поправила голосовалку

--- src/panels/Golos.js
+++ src/panels/Golos.js
@@ -6,64 +6,33 @@ import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
-import ArticleList from "../components/ArticleList"
-// import articles from "../components/fixtures"
 import CellButton from '@vkontakte/vkui/dist/components/CellButton/CellButton';
 import Icon16Add from '@vkontakte/icons/dist/16/add';
-import Icon16Search from '@vkontakte/icons/dist/16/search';
-// import IconNews from '@vkontakte/icons/dist/20/newsfeed_outline';
-import Separator from '@vkontakte/vkui/dist/components/Separator/Separator';
-// import View from '@vkontakte/vkui/dist/components/View/View';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Header from '@vkontakte/vkui/dist/components/Header/Header';
-// import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Textarea from '@vkontakte/vkui/dist/components/Textarea/Textarea';
-import Checkbox from '@vkontakte/vkui/dist/components/Checkbox/Checkbox';
-// import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
 import variants1 from "../components/vars.json"
 import './Student.css';
 
 function Golos(props) {
+	let stat = {
+		vote: false
+	
+	}
 const [variants, setVariants] = React.useState(variants1)
+const [state, setState] = React.useState(stat)
 const osName = platform();
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
   }
 
-// const handleClick = () => {
-// 	console.log('---', 'clicked')
-// 	let inf = { //посмотреть что с ключами(id)
-// 		id:   getRandomInt(30, 1000000), date: "2020-12-06", 
-// 		title: "Практическая работа", 
-// 		text: "Практическая работа по экономике"
-// 	}	
-// 	if (text === 'Сортировать: сначала новые') {
-// 		setArticles(
-// 			articles.concat([inf])
-// 		  )
-// 	}
-// 	else {
-// 		setArticles(
-// 			articles.reverse().concat([inf]).reverse()
-// 	  )
-// 	}
-// };
-
-
-let state = {
-    vote: false
-
-}
-
-
 const voted = (VarId) => {
-    state.vote = true;
-    variants[VarId].count=variants[VarId].count+1;
-    console.log(state.reverted)
+    setState(state.vote,true);
+	variants[VarId].count=variants[VarId].count+1;
+	setVariants(variants)
 	}
 
-//добавление новости
 function addTodo(titleIn) {
     
 	let inf = { //посмотреть что с ключами(id)
@@ -73,7 +42,6 @@ function addTodo(titleIn) {
 	}
 		setVariants(variants.concat([inf]))
   }
-
 
 return (
 	<Panel id={props.id}>
